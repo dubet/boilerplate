d139f4708415de7df092babdfe4e31799bb90aae
Cyril Dubet <56584089+dubet@users.noreply.github.com>
2022-08-21 02:16:29 +0200

    Add various improvements (#4)

--- Tests/BoilerPlate/Test.cpp
+++ Tests/BoilerPlate/Test.cpp
@@ -1,11 +1,13 @@
 #include <gtest/gtest.h>
 #include <BoilerPlate/Boilerplate.h>
 
-TEST(FooTest, BasicAssertion) {
+TEST(FooTest, BasicAssertion)
+{
     EXPECT_EQ(BoilerPlate::Foo(1, 1), 2);
 }
 
-TEST(BarTest, BasicAssertion) {
+TEST(BarTest, BasicAssertion)
+{
     EXPECT_EQ(BoilerPlate::Bar(2, 2), 0);
 }
 
